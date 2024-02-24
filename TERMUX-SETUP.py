a7c0f5c447ef9d90d42913200951a83e63d425bf
ALHAJ-404 <161096041+ALHAJ-404@users.noreply.github.com>
2024-02-24 16:21:24 +0600

    Add files via upload

---
+++ TERMUX-SETUP.py
@@ -0,0 +1,142 @@
+# == [ STORAGE USE : 1.6 GB ] == #
+
+import os, sys
+
+print("INSTALL ALL")
+
+
+os.system("pkg update -y")
+os.system("pkg upgrade -y")
+os.system("pkg install python")
+
+os.system("pkg install python2")
+
+os.system("pkg install python3")
+
+os.system("pip install rich")
+
+os.system("pip install requests")
+
+os.system("pip2 install requests")
+
+os.system("pip install mechanize")
+
+os.system("pip2 install mechanize")
+
+os.system("pip install lolcat")
+
+os.system("pip install bs4")
+
+os.system("pip2 install bs4")
+
+os.system("pip install futures")
+
+os.system("pip2 install futures")
+
+os.system("pkg install git")
+
+# END 
+
+
+
+🔰TERMUX SETUP COMMAND🔰
+♻️TERMUX ERROR FIXED ♻️
+
+For Server Change 
+
+termux-change-repo
+
+Mirror grouRotate 👈
+Selected This server
+
+TERMUX FULL SET-UP COMAND FOR TERMUX USER:-
+
+pkg update
+pkg upgrade
+pkg install python
+pkg install python2
+pkg install python3
+pkg install python-pip
+pkg install wget
+pkg install fish
+pkg install ruby
+pkg install help
+pkg install git
+pkg install dnsutils  
+pkg install php
+pkg install perl
+pkg install lua
+pkg install parallel
+pkg install nmap
+pkg install bash
+pkg install clang
+pkg install nano
+pkg install w3m
+pkg install hydra
+pkg install figlet
+pkg install cowsay
+pkg install curl
+pkg install tar
+pkg install zip
+pkg install unzip
+pkg install net-tools
+pkg install tor -y
+pkg install sudo -y
+pkg install wireshark
+pkg install wgetrc
+pkg install wcalc
+pkg install openssl
+pkg install openssl-tool
+pkg install bmon
+pkg install vpn
+pkg install unrar
+pkg install toilet
+pkg install proot
+pkg install net-tools
+pkg install vim
+pkg install vim-python
+pkg install ired
+pkg install goaccess
+pkg install golang
+pkg install tar
+pkg install kibi
+pkg install tsu
+pkg install tmux
+pkg install mtools
+pkg install file
+pkg install vis
+pkg install pass
+pkg install pick
+pkg install chroot
+termux-chroot
+pkg install macchanger
+pkg install ninja
+pkg install elixir
+pkg install swift
+pkg install xmlstarlet
+pkg install fakeroot
+pkg install texinfo
+pkg install netcat
+pkg install wren
+pkg install gatling
+pkg install cvs
+pkg install ffmpeg
+pkg install screen 
+pkg install neofetch 
+pkg install mariadb
+pkg install picolisp
+pkg install toilet
+pkg install cmatrix
+pkg install dropbear
+pkg install openssh
+pkg install python-pip
+pip2 install wget
+pip install bs4
+pip2 install bs4
+pip install requests
+pip2 install requests
+pip install mechanize
+pip2 install mechanize
+pip install php
+pip2 install php
+
